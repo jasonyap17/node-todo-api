197256430abecbcc6b6fa8c87d8d2c70673c6482
Jason Yap <jason.yap@kp.org>
2018-11-02 12:04:38 -0700

    Testing todos

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 })
 
+module.exports = {
+    app
+};
 // var saveUser = (user) => {
 //     user.save()
 //     .then((doc) => console.log('Saved user', doc))
