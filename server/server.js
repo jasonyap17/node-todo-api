45f97b06e399e8eb9846e26964a52ff88eb452c3
Jason Yap <jason.yap@kp.org>
2018-11-02 14:00:52 -0700

    GET todos/id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -22,6 +24,18 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }).catch((e) => res.status(400).send(err));    
+});
+
+app.get('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } 
+        res.send({todo});
+    }).catch((err) => res.status(400).send(err));
 });
 
 app.listen(3000, () => {
