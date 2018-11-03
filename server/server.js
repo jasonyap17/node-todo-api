1456e00ddc7494acac290d1435432d5968824870
Jason Yap <jason.yap@kp.org>
2018-11-02 20:58:49 -0700

    Getting ready to heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -38,8 +39,8 @@ app.get('/todos/:id', (req, res) => {
     }).catch((err) => res.status(400).send(err));
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 })
 
 module.exports = {
