8197efed7a8c8b07db6ff54d8ff4b926a1a7b2d1
Jason Yap <jason.yap@kp.org>
2018-11-02 22:00:21 -0700

    Added patch method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
@@ -52,6 +53,30 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({ todo });
+        }).catch((err) => res.status(400).send(err));
+
+});
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
