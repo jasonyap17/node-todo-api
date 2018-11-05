38d3db8721079dbb982111794fde0e837b3d6f49
Jason Yap <jason.yap@kp.org>
2018-11-05 10:26:26 -0800

    Add GET users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {mongoose} = require('./db/mongoose');
 
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const {ObjectID} = require('mongodb');
 
@@ -93,6 +94,11 @@ app.post('/users', (req, res) => {
             res.header('x-auth', token).send(user)
         })
         .catch((err) => res.status(400).send(err));
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
